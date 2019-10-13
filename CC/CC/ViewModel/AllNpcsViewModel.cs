d06915bc06737de6468eeab09cd704bbe9b4e880
Shaika Khan <ShaikaK@intergen.org.nz>
2019-10-14 10:09:59 +1300

    putting project back

---
+++ CC/CC/ViewModel/AllNpcsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CC.ViewModel
+{
+    class AllNpcsViewModel : ContentPage
+    {
+        public AllNpcsViewModel()
+        {
+            Title = "Contacts Page";
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label {
+                        Text = "Contacts data goes here",
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ CC/CC/Views/MasterPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using CC.ViewModel;
+
+namespace CC.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : ContentPage
+    {        
+
+        public MasterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
